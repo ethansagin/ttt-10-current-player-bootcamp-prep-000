86c108979156f239b28ca85d2cdf3c62ed39bff1
Ethan <ethansagin@gmail.com>
2019-02-26 21:20:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   board.each do |space|
-    turn_counter = 0
+    counter = 0
     if space == "X" || space == "O"
-      turn_counter += 1
+      counter += 1
     end
-    turn_counter
+    counter
   end
 end
